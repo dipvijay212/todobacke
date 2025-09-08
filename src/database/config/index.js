686395797947c769ba860fda4bcdf1e932f54313
dipvijay212 <148552241+dipvijay212@users.noreply.github.com>
2025-09-08 10:45:19 +0530

    Update index.js

--- src/database/config/index.js
+++ src/database/config/index.js
@@ -1,11 +1,24 @@
-
 module.exports = {
   development: {
-    username: "root",
-    password: "Admin@123",
-    database: "practice",
-    host: "127.0.0.1",
-    port: 3306,
-    dialect: "mysql",
+    username: "tododb_lnoy_user",
+    password: "CFjOBgimI24EIXdweceS2f0JDJzlsyh2",
+    database: "tododb_lnoy",
+    host: "dpg-d2v66lmr433s73et07ig-a",
+    port: 5432,
+    dialect: "postgres",
+  },
+  production: {
+    username: "tododb_lnoy_user",
+    password: "CFjOBgimI24EIXdweceS2f0JDJzlsyh2",
+    database: "tododb_lnoy",
+    host: "dpg-d2v66lmr433s73et07ig-a",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
